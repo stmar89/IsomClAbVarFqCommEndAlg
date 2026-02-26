60c464e888c8679ab38bbb601e764e2bc0feef6c
stmar89 <stefano.marseglia89@gmail.com>
2026-02-26 18:07:32 +0100

    trying to compute delta. no success yet

--- DieudonneModules.m
+++ DieudonneModules.m
@@ -436,11 +436,15 @@ element alpha of OA, as reqired by Algorithm 2 of the paper, to define the reduc
             val_nu:=Valuation(pi,nu); // in E
             w_nu:=pi/(t_nu^val_nu); // in E 
             assert Valuation(w_nu,nu) eq 0;
-            // FIXME t_nu is constructed using a CRT method. So, when dividing by it, it might not lead to the a unit mod nu.
-            // But pi is exact. So instead we construct t_nu^var(pi)/pi, pull it back, and then invert again.
-            //wU:=U_pr(Delta_map(w_nu)); // in E->A->U
+            // wU:=U_pr(Delta_map(w_nu)); // in E->A->U
+            // The line above is affected by the following issue: t_nu is a uniformizer at nu and a 
+            // unit every over place above p. But its lift to OA might be contained in some maximal ideal
+            // not containing p. It follows that pi/t_nu^val_nu might not be a in OA. Hence it cannot
+            // be pushed by U_pr in the unit group. 
+            // Workaround: the element t_nu^var(pi)/pi is integral and the RayClassGroup map can detect 
+            // that it is a unit modulo the ideal we are quotienting with (do not know how. it is in the kernel of Magma).
+            // In other words, we take the inverse of the inverse.
             wU:=-U_pr(Delta_map(w_nu^-1)); // in E->A->U
-"wU OK";
             gamma0:=wU@@phi; // in Us[g_nu], the last componenet of U
             gamma_A:=(&+[i lt g_nu select 
                                     U_embs[i](One(A)@@us_nu[i]) else 
@@ -459,33 +463,64 @@ element alpha of OA, as reqired by Algorithm 2 of the paper, to define the reduc
             return alpha_nu,PPs_nu_m_prod,PPs_nu_m,Us_nu[g_nu],us_nu[g_nu];
         end function;
 
-        delta_nu_at_precision:=function(nu,alpha_nu,alpha_onu,sigma,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu)
+        delta_nu_at_precision_conj_stable:=function(nu,alpha_nu,sigma,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu)
+        // TODO update this description
+        // input: 
+        // output: 
+            f_nu:=InertiaDegree(nu);
+            g_nu:=GCD(a,f_nu); //q=p^a
+            pg_nu:=p^g_nu;
+            // sigma^g_nu = tau
+            tau:=iso<U_gnu->U_gnu|[U_gnu.i@u_gnu@qI@(sigma^g_nu)@@qI@@u_gnu : i in [1..Ngens(U_gnu)]]>;
+            assert forall{i:i in [1..Ngens(U_gnu)]|(tau^(Integers()!(a/g_nu)))(U_gnu.i) eq U_gnu.i};
+            // we construct the hom x:->tau(x)/x
+            tau_over_id:=hom<U_gnu->U_gnu|[ (U_gnu.i@tau)-U_gnu.i : i in [1..Ngens(U_gnu)]]>;
+
+            assert forall{i:i in [1..g_nu-1]|alpha_nu-1 in PPs_nu_m[i]};
+            // FIXME something broken here ... :-(
+            u_nu:=&*[alpha_nu@qI@(sigma^i)@@qI:i in [0..g_nu-1]]; // = (u_nu,...,u_nu) in A
+[Valuation(u_nu,P):P in primes_of_A_above_place_of_E(A,nu)];
+            bar_u_nu:=bar_onA(u_nu);
+[Valuation(bar_u_nu,P):P in primes_of_A_above_place_of_E(A,nu)];
+            UU:=u_nu*bar_u_nu/pg_nu;
+[Valuation(UU,P):P in primes_of_A_above_place_of_E(A,nu)];
+            assert Valuation(UU,P) eq 0 where P:=primes_of_A_above_place_of_E(A,nu)[g_nu];
+            UU:=UU@@u_gnu; // in U_gnu
+            // FIXME next assertion is failing for some reason
+            assert &+[UU@(tau^i):i in [0..(a div g_nu)-1]] eq Zero(U_gnu); // Norm of UU is 1.
+            delta1:=UU@@tau_over_id@u_gnu; //in A
+            
+            rho_g_nu:=Index(PPs_nu_m,Ideal(OA,[bar_onA(z):z in ZBasis(PPs_nu_m[g_nu])]));
+            delta_nu:=[delta1*p^-(i-1):i in [1..rho_g_nu]] cat [delta1*bar_u_nu*p^-(i-1):i in [rho_g_nu+1..g_nu]];
+            delta_nu:=[pg_nu*x:x in delta_nu]; // to make sure that it is in OA
+            assert forall{x:x in delta_nu|x in OA};
+            delta_nu:=pg_nu^-1 * CRT(PPs_nu_m,delta_nu); //in A
+
+            // to make sure that the lift is not a zero divisor
+            while IsZeroDivisor(delta_nu) do
+                delta_nu+:=Random(PPs_nu_m_prod);
+            end while;
+            return delta_nu;
+        end function;
+
+        delta_nu_at_precision_conj_pair:=function(nu,alpha_nu,alpha_onu,sigma,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu)
         // TODO update this description
         // input: 
         // output: 
             f_nu:=InertiaDegree(nu);
             g_nu:=GCD(a,f_nu); //q=p^a
             // sigma^g_nu = tau
-            tau:=iso<U_gnu->U_gnu|[ U_gnu.i@u_gnu@qI@(sigma^g_nu)@@qI@@u_gnu : i in [1..Ngens(U_gnu)]]>;
-            assert forall{i:i in [1..Ngens(U_gnu)]|(tau^(Integers()!(a/g_nu)))(U_gnu.i) eq U_gnu.i};
+            tau:=iso<U_gnu->U_gnu|[U_gnu.i@u_gnu@qI@(sigma^g_nu)@@qI@@u_gnu : i in [1..Ngens(U_gnu)]]>;
+            assert2 forall{i:i in [1..Ngens(U_gnu)]|(tau^(Integers()!(a/g_nu)))(U_gnu.i) eq U_gnu.i};
             // we construct the hom x:->tau(x)/x
             tau_over_id:=hom<U_gnu->U_gnu|[ (U_gnu.i@tau)-U_gnu.i : i in [1..Ngens(U_gnu)]]>;
-printf "conj_stable = %o , slope = %o, g_nu = %o , f_nu = %o\n",IsConjugateStable(nu),Slope(nu),g_nu,f_nu;
-printf "[P=bar(P):P|nu]=%o\n",[P eq Ideal(OA,[bar_onA(z):z in ZBasis(P)]):P in primes_of_A_above_place_of_E(A,nu)];
             UU:=alpha_nu*bar_onA(alpha_onu)/p^g_nu;
-[Valuation(alpha_nu,P) : P in primes_of_A_above_place_of_E(A,nu)];
-[Valuation(bar_onA(alpha_onu),P) : P in primes_of_A_above_place_of_E(A,nu)];
-[Valuation(alpha_nu*bar_onA(alpha_onu),P) : P in primes_of_A_above_place_of_E(A,nu)];
-[Valuation(A!(p^g_nu),P) : P in primes_of_A_above_place_of_E(A,nu)];
-[Valuation(UU,P) : P in primes_of_A_above_place_of_E(A,nu)];
             assert Valuation(UU,P) eq 0 where P:=primes_of_A_above_place_of_E(A,nu)[g_nu];
-            //UU:=UU@@u_gnu; // in U_gnu //FIXME 20250219 same comment as above, same workaround.
-            UU:=-(UU^-1)@@u_gnu;
-"UU OK";
+            UU:=UU@@u_gnu; // in U_gnu
             assert &+[UU@(tau^i):i in [0..(a div g_nu)-1]] eq Zero(U_gnu);
-//UU@@tau_over_id;
-            delta1:=UU@u_gnu; //in A
-            delta_nu:=CRT(PPs_nu_m,[delta1*p^(i-1): i in [1..g_nu]]); //in A
+            delta1:=UU@@tau_over_id@u_gnu; //in A
+            // We first multiply by p^g_nu to guarantee that the elements are in OA, apply CRT and then divide by p^g_nu.
+            delta_nu:=p^-g_nu*CRT(PPs_nu_m,[delta1*p^(g_nu-(i-1)): i in [1..g_nu]]); //in A
 
             // to make sure that the lift is not a zero divisor
             while IsZeroDivisor(delta_nu) do
@@ -546,23 +581,27 @@ printf "[P=bar(P):P|nu]=%o\n",[P eq Ideal(OA,[bar_onA(z):z in ZBasis(P)]):P in p
                         : nu in places_considered ]);
                 m:=Max(m,m2);
                 I:=p^m*OA;
-"m=",m;
+//"m=",m;
                 QI,qI:=ResidueRing(OA,I);
                 sigma_m:=sigma_OA_mod_I(QI,qI,A);
                 for inu in conj_stable_indices do
                     vprintf alpha_at_precision,1 : "Computing alpha at precision %o for the %o-th place",m,inu;
                     nu:=places_considered[inu];
+                    f_nu:=InertiaDegree(nu);
+                    g_nu:=GCD(a,f_nu); //q=p^a
                     t_nu:=uniformizers_at_nus[inu];
                     alpha_nu,PPs_nu_m_prod,PPs_nu_m,U_gnu,u_gnu:=alpha_at_precision_W_type_at_place(m,nu,sigma_m,t_nu,qI);
                     assert not IsZeroDivisor(alpha_nu);
-                    delta_nu:=delta_nu_at_precision(nu,alpha_nu,alpha_nu,sigma_m,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu);
-                    output[nu]:=<alpha_nu,PPs_nu_m_prod,delta_nu>;
+                    delta_nu:=delta_nu_at_precision_conj_stable(nu,alpha_nu,sigma_m,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu);
+                    output[nu]:=<alpha_nu,PPs_nu_m_prod,delta_nu,g_nu>;
                     vprintf alpha_at_precision,1 : "done\n";
                 end for;
                 for pair in conj_pairs_indices do
                     inu,inu_bar:=Explode(pair);
                     vprintf alpha_at_precision,1 : "Computing alpha at precision %o for the (%o,%o)-th places",m,inu,inu_bar;
                     nu:=places_considered[inu];
+                    f_nu:=InertiaDegree(nu);
+                    g_nu:=GCD(a,f_nu); //q=p^a
                     t_nu:=uniformizers_at_nus[inu];
                     nu_bar:=places_considered[inu_bar];
                     t_nu_bar:=uniformizers_at_nus[inu_bar];
@@ -572,14 +611,14 @@ printf "[P=bar(P):P|nu]=%o\n",[P eq Ideal(OA,[bar_onA(z):z in ZBasis(P)]):P in p
                     assert not IsZeroDivisor(alpha_nu_bar);
                     // The places of A above nu and nu_Bar are sorted compatibly according to complex conjugation in the
                     // calls of PlacesOfAAbove done in alpha_at_precision_W_type_at_place.
-                    // This is needed for the notion of Wtype at nu is compatible with the notion of Wtype at nu_bar.
-                    delta_nu:=delta_nu_at_precision(nu,alpha_nu,alpha_nu_bar,sigma_m,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu);
-print "delta_nu OK";
-                    delta_nu_bar:=delta_nu_at_precision(nu_bar,alpha_nu_bar,alpha_nu,sigma_m,qI,PPs_nu_bar_m,PPs_nu_bar_m_prod,U_gnu_bar,u_gnu_bar);
-print "delta_nu_bar OK";
+                    // This is needed for the notion of Wtype at nu to be compatible with the notion of Wtype at nu_bar.
+                    delta_nu:=delta_nu_at_precision_conj_pair(nu,alpha_nu,alpha_nu_bar,sigma_m,qI,PPs_nu_m,PPs_nu_m_prod,U_gnu,u_gnu);
+//print "delta_nu OK";
+                    delta_nu_bar:=delta_nu_at_precision_conj_pair(nu_bar,alpha_nu_bar,alpha_nu,sigma_m,qI,PPs_nu_bar_m,PPs_nu_bar_m_prod,U_gnu_bar,u_gnu_bar);
+//print "delta_nu_bar OK";
                     assert PPs_nu_bar_m_prod eq Ideal(OA,[bar_onA(z):z in ZBasis(PPs_nu_m_prod)]);
-                    output[nu]:=<alpha_nu,PPs_nu_m_prod,delta_nu>;
-                    output[nu_bar]:=<alpha_nu_bar,PPs_nu_bar_m_prod,delta_nu_bar>;
+                    output[nu]:=<alpha_nu,PPs_nu_m_prod,delta_nu,g_nu>;
+                    output[nu_bar]:=<alpha_nu_bar,PPs_nu_bar_m_prod,delta_nu_bar,g_nu>;
                     vprintf alpha_at_precision,1 : "done\n";
                 end for;
             end if;
@@ -1039,7 +1078,8 @@ The VarArg DualsCompatible determines whether the SemilinearOperators F,V are co
     vprintf Algorithm_3,2 : "\n";
    
     if DualsCompatible then
-        isog`delta_Hilbert90:=CRT([nu[2]:inu->nu in alpha_arr],[nu[3]:inu->nu in alpha_arr]);
+        p_max_g_nu:=p^Max([nu[4]:inu->nu in alpha_arr]); //p^N*delta_nu is integral, for every nu
+        isog`delta_Hilbert90:=p_max_g_nu^-1*CRT([nu[2]:inu->nu in alpha_arr],[p_max_g_nu*nu[3]:inu->nu in alpha_arr]);
     end if;
     return Delta_isom_classes_WR_F_V;
 end intrinsic;
