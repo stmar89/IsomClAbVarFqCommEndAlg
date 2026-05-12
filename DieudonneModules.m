6a9d7bb755dffc3014f6b99edd4bc7259283da9f
stmar89 <stefano.marseglia89@gmail.com>
2026-05-12 14:44:38 +0200

    moved SemilinearOperators to its own file

--- DieudonneModules.m
+++ DieudonneModules.m
@@ -32,8 +32,7 @@ declare verbose sigma,3;
 declare verbose alpha_at_precision,3;
 declare verbose Delta_scaling,3;
 
-declare attributes IsogenyClassFq : DiedudonneAlgebraCommEndAlg,
-                                    SemilinearOperators;
+declare attributes IsogenyClassFq : DiedudonneAlgebraCommEndAlg;
                                
 declare attributes AlgEtQ         : sigma_fin_prec,
                                     PlacesAboveRationalPrime;
@@ -484,26 +483,6 @@ intrinsic DieudonneAlgebraCommEndAlg(isog::IsogenyClassFq)->FldNum,RngOrd,RngOrd
     end if;
     return Explode(isog`DiedudonneAlgebraCommEndAlg);
 end intrinsic;
-
-////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////SemilinearOperators /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-
-intrinsic SemilinearOperators(isog::IsogenyClassFq)->RngIntElt,AlgEtQIdl,AlgEtQIdl,GrpAb,Map,Map,Map
-{Returns the homonymous attribute of the isogeny class, which consists of the following informations: m0,J,den_ideal,Qm0,qm0,FQm0,VQm0, where (see DieudonneAlgebraCommEndAlg for the missing definitions):
-- m0 is a positive integer;
-- J is a WR-ideal with maximal endomorphism ring OA which is stable under the action of F and V=pF^-1, for some semilinear operator F with the Frobenius property of and of W-type;
-- den_ideal = p^m0*J+P01^M*J, where P01 is the product of the maximal ideals of WR which are above the unique local-local maximal ideal of R, and M is chosen so that P01^MJ c J locally at every such maximal ideal;
-- Qm0 is the abelian group J/den_ideal and qm0 is the quotient map J->Qm0;
-- FQm0 and Vm0 are additive maps induced by semilinear operators F and V as above. They represent the Frobenius and Verschiebung acting on Dieudonné modules.
-The attribute SemilinearOperators needs to be computed beforehand, during a run of IsomorphismClassesDieudonneModulesCommEndAlg. During this run, the integer m0 is automatically computed by the program to guarantee that every fractional W'R-ideal I' whose extension to OA is F and V stable such that I' c J and den_ideal c I'. These two conditions allow us to verify if I' is a W'R\{F,V\} ideal, that is, (the local-local-part) of a Dieudonne module of some abelian variety in isog.}
-    //TODO: in the future it would be nice to be able to increase the precision with a call of this intrinsic.
-    // This would require to choose a new alpha (as in alpha_at_precision) 'compatible' with the current choice.
-    // I might be a bit complicated to code, and it is outside of the current scope.
-    require assigned isog`SemilinearOperators : "Run first IsomorphismClassesDieudonneModules(isog)";
-    return Explode(isog`SemilinearOperators);
-end intrinsic;
-
 
 ////////////////////////////////////////////////////////////////////////////////////
 //////////////////////// IsomorphismClassesDieudonneModules ////////////////////////
