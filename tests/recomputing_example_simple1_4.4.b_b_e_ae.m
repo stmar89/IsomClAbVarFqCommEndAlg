29ee60f55f2f1ff07ecdaf08859cff9c84abb56f
stmar89 <stefano.marseglia89@gmail.com>
2026-05-19 10:50:32 +0200

    added test (0,1) vs all approaches

--- tests/recomputing_example_simple1_4.4.b_b_e_ae.m
+++ tests/recomputing_example_simple1_4.4.b_b_e_ae.m
@@ -20,59 +20,69 @@
 
     h:=x^8+x^7+x^6+4*x^5-4*x^4+16*x^3+16*x^2+64*x + 256;
     assert IsSquarefree(h);
-    isog:=IsogenyClass(h);
-    g:=Dimension(isog);
-    q:=FiniteField(isog);
-    t,p,a:=IsPrimePower(q); assert t;
 
-    t0:=Cputime();
-    iso:=IsomorphismClasses(isog);
-    t1:=Truncate(Cputime(t0)) div 60;
-    printf "We got %o isomorphism classes in %o minutes\n",#iso,t1;
+    for slopes in ["(0,1)","all"] do
+        isog:=IsogenyClass(h);
+        g:=Dimension(isog);
+        q:=FiniteField(isog);
+        t,p,a:=IsPrimePower(q); assert t;
+        t0:=Cputime();
+        iso:=IsomorphismClassesCommEndAlg(isog:slopesDieudonneModules:=slopes);
+        t1:=Truncate(Cputime(t0)) div 60;
+        printf "Using slopesDieudonneModules:=%o, we got %o isomorphism classes in %o minutes\n",slopes,#iso,t1;
 
-    R:=ZFVOrder(isog);
-    E:=Algebra(R);
+        R:=ZFVOrder(isog);
+        E:=Algebra(R);
 
-    oo:=OverOrders(R);
-    OE:=MaximalOrder(E);
-    _,P,_:=PrimesOfZFVAbove_p(isog);
-    assert #P eq 1;
-    P:=P[1];
-    // P is the local-local maximal ideal of R above p
+        oo:=OverOrders(R);
+        OE:=MaximalOrder(E);
+        _,P,_:=PrimesOfZFVAbove_p(isog);
+        assert #P eq 1;
+        P:=P[1];
+        // P is the local-local maximal ideal of R above p
 
-    is_maximal_at_01:=function(S)
-    // check if the overorder S of R is maximal locally at its local-local part.
-        return S!!OneIdeal(OE) eq OneIdeal(S) + S!!OE!!P;
-    end function;
+        is_maximal_at_01:=function(S)
+        // check if the overorder S of R is maximal locally at its local-local part.
+            return S!!OneIdeal(OE) eq OneIdeal(S) + S!!OE!!P;
+        end function;
 
-    Ep,mEp:=TotallyRealSubAlgebra(E);
-    OEp:=MaximalOrder(Ep);
-    "p is " cat (#PlacesAboveRationalPrime(Ep,p) eq g select "" else "not ") cat "totally split in E^+";
-    OEp:=[mEp(z):z in ZBasis(OEp)];
-    contains_OEp:=func< S | forall{z:z in OEp|z in S}>;
+        Ep,mEp:=TotallyRealSubAlgebra(E);
+        OEp:=MaximalOrder(Ep);
+        "p is " cat (#PlacesAboveRationalPrime(Ep,p) eq g select "" else "not ") cat "totally split in E^+";
+        OEp:=[mEp(z):z in ZBasis(OEp)];
+        contains_OEp:=func< S | forall{z:z in OEp|z in S}>;
 
 
-    Q,mQ,F,V,_,_,J:=Explode(isog`SemilinearOperatorsWType);
-    ind:=[Index(OE,S):S in oo];
-    ParallelSort(~ind,~oo);
-    Reverse(~oo);
-    ends:=[ EndomorphismRing(A) : A in iso ];
-    printf "For each overorder S, we print the following string of data:\n\tiS = which overorder of Z[pi,q/pi]\n\t[OE:S]\n\td(S) = #Dieudonné modules with End S\n\th(S)=#Pic(S)\n\ta numbers of the DM with End S\n\tis S maximal at (0,1)?\n\tdoes S contain O_{E^+}?\n\tindices of minimal overorders\n\n";
-    for iS->S in oo do
-        dmS:={@ dmA where _,dmA:=IsomDataCommEndAlg(A) : A in iso | EndomorphismRing(A) eq S @};
-        // a-numbers
-        a_nums:=[];
-        for dm in dmS do
-            assert dm subset J;
-            M:=sub<Q|[mQ(z):z in ZBasis(dm)]>;
-            FM:=sub<M|[M!F(M.i):i in [1..Ngens(M)]]>;
-            VM:=sub<M|[M!V(M.i):i in [1..Ngens(M)]]>;
-            Append(~a_nums,Ilog(q,Index(M,FM+VM)));
+        Q,mQ,F,V,_,_,J:=SemilinearOperators(isog);
+        ind:=[Index(OE,S):S in oo];
+        ParallelSort(~ind,~oo);
+        Reverse(~oo);
+        ends:=[ EndomorphismRing(A) : A in iso ];
+        printf "For each overorder S, we print the following string of data:\n\tiS = which overorder of Z[pi,q/pi]\n\t[OE:S]\n\td(S) = #Dieudonné modules with End S\n\th(S)=#Pic(S)\n\ta numbers of the DM with End S\n\tis S maximal at (0,1)?\n\tdoes S contain O_{E^+}?\n\tindices of minimal overorders\n\n";
+        for iS->S in oo do
+            dmS:={@ dmA where _,dmA:=IsomDataCommEndAlg(A) : A in iso | EndomorphismRing(A) eq S @};
+            // a-numbers
+            a_nums:=[];
+            for dm in dmS do
+                assert dm subset J;
+                M:=sub<Q|[mQ(z):z in ZBasis(dm)]>;
+                FM:=sub<M|[M!F(M.i):i in [1..Ngens(M)]]>;
+                VM:=sub<M|[M!V(M.i):i in [1..Ngens(M)]]>;
+                Append(~a_nums,Ilog(q,Index(M,FM+VM)));
+            end for;
+            // indices of minimal overorders (to find the place of S in the graph of inclusions)
+            ind_min_oo:=[ Index(oo,T) : T in MinimalOverOrders(S) ];
+            printf "%o,%o,%o,%o,%o,%o,%o,%o\n",iS,Index(OE,S),#dmS,#PicardGroup(S),a_nums,is_maximal_at_01(S),contains_OEp(S),ind_min_oo;
         end for;
-        // indices of minimal overorders (to find the place of S in the graph of inclusions)
-        ind_min_oo:=[ Index(oo,T) : T in MinimalOverOrders(S) ];
-        printf "%o,%o,%o,%o,%o,%o,%o,%o\n",iS,Index(OE,S),#dmS,#PicardGroup(S),a_nums,is_maximal_at_01(S),contains_OEp(S),ind_min_oo;
+       
+        t0:=Cputime();
+        gen_del_mods:=[GeneralizedDeligneModule(A):A in iso];
+        t1:=Round(Cputime(t0)) div 60;
+        printf "Computation of GeneralizedDeligneModules done in %o minutes\n\n\n\n",t1;
+
+        delete isog; //for the second run
     end for;
+
 
 /*
 
