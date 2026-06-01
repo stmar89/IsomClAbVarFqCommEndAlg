eb6b8c76623bfb4bd0f9f290d96ac81d8e251f3d
stmar89 <stefano.marseglia89@gmail.com>
2026-06-01 17:09:23 +0200

    resolved FIXME

--- CreationAbVarFq.m
+++ CreationAbVarFq.m
@@ -48,45 +48,73 @@ intrinsic AbelianVarietyCommEndAlg(isog::IsogenyClassFq,tup:Tup)->AbelianVariety
 returns the unique abelian variety in isog with EndomorphismRing S whose l-Tate modules are isomorphic to I (for all l neq p), the étale-local and local-étale part of the Dieudonné module are determined by I, while the local-local part is determined by M, and L determines its position in the orbit of the class group of S acting on the local information just described.}
     require IsSquarefree(isog) : "The isogeny class needs to have squarefree Weil polynomial.";
     R:=ZFVOrder(isog);
-    I,M,L,S:=Explode(tup);
+    I,M,L,S,slopes:=Explode(tup);
     require R eq Order(I): "I is not a ZFV-ideal.";
     require R subset S : "S cannot be an endomorphism ring.";
     require Order(L) eq S and IsInvertible(L): "L needs to be an invertible ideal of S";
     if GetAssertions() ge 2 then
         _,_,_,_,_,_,_,Delta_map,WR:=DieudonneAlgebraCommEndAlg(isog);
         require Order(M) eq WR : "M needs to be a WR-ideal";
-// FIXME this test assume (0,1)... need to update or fix
-//        if GetAssertions() ge 3 then
-//            //TEST consistency of S with the local data. Very time consuming
-//            E:=Algebra(R);
-//            places_0,places_01,places_1:=PrimesOfZFVAbove_p(isog);
-//            places_away_01:=SingPrimesOfZFVAwayFrom_p(isog) cat [P:P in places_0|not IsInvertible(P)] cat [P:P in places_0|not IsInvertible(P)];
-//            OE:=MaximalOrder(E);
-//            end_test:=[];
-//            for T in OverOrders(R) do
-//                mT:=R!!OneIdeal(T);
-//                if #places_away_01 gt 0 then
-//                    IT:=R!!(T!!I);
-//                    I_IT:=I+IT;
-//                    sendsItoI:=forall{P : P in places_away_01 | I_IT eq I + P*I_IT }; 
-//                else
-//                    sendsItoI:=true; 
-//                end if;
-//                if #places_01 gt 0 then
-//                    MT:=Ideal(WR,[ Delta_map(t)*m : t in ZBasis(T) , m in ZBasis(M) ]);
-//                    M_MT:=M+MT;
-//                    assert assigned WR`PrimesOfSlopeIn01;
-//                    sendsMtoM:=forall{ P : P in WR`PrimesOfSlopeIn01 | M_MT eq M + P*(M_MT) };
-//                    // M = MT at (0,1)
-//                else
-//                    sendsMtoM:=true;
-//                end if;
-//                if sendsItoI and sendsMtoM then
-//                    Append(~end_test,T);
-//                end if;
-//            end for;
-//            require S eq Order(&cat[ ZBasis(T) : T in end_test ]) : "The local info provided by I and M determine and order which is different from S.";
-//        end if;
+        if GetAssertions() ge 3 then
+            //TEST consistency of S with the local data. Very time consuming
+            if slopes eq "(0,1)" then
+                E:=Algebra(R);
+                places_0,places_01,places_1:=PrimesOfZFVAbove_p(isog);
+                places_away_01:=SingPrimesOfZFVAwayFrom_p(isog) cat [P:P in places_0|not IsInvertible(P)] cat [P:P in places_0|not IsInvertible(P)];
+                end_test:=[];
+                for T in OverOrders(R) do
+                    mT:=R!!OneIdeal(T);
+                    if #places_away_01 gt 0 then
+                        IT:=R!!(T!!I);
+                        I_IT:=I+IT;
+                        sendsItoI:=forall{P : P in places_away_01 | I_IT eq I + P*I_IT }; 
+                    else
+                        sendsItoI:=true; 
+                    end if;
+                    if #places_01 gt 0 then
+                        MT:=Ideal(WR,[ Delta_map(t)*m : t in ZBasis(T) , m in ZBasis(M) ]);
+                        M_MT:=M+MT;
+                        assert assigned WR`PrimesOfSlopeIn01;
+                        sendsMtoM:=forall{ P : P in WR`PrimesOfSlopeIn01 | M_MT eq M + P*(M_MT) };
+                        // M = MT at (0,1)
+                    else
+                        sendsMtoM:=true;
+                    end if;
+                    if sendsItoI and sendsMtoM then
+                        Append(~end_test,T);
+                    end if;
+                end for;
+                require S eq Order(&cat[ ZBasis(T) : T in end_test ]) : "The local info provided by I and M determine and order which is different from S.";
+            else
+                assert slopes eq "all";
+                E:=Algebra(R);
+                p:=CharacteristicFiniteField(isog);
+                places_away_01:=SingPrimesOfZFVAwayFrom_p(isog);
+                end_test:=[];
+                for T in OverOrders(R) do
+                    mT:=R!!OneIdeal(T);
+                    if #places_away_01 gt 0 then
+                        IT:=R!!(T!!I);
+                        assert I subset IT;
+                        sendsItoI:=IsPowerOf(Index(IT,I),p); 
+                        // I = IT away from p
+                    else
+                        sendsItoI:=true; 
+                    end if;
+                    if #places_01 gt 0 then
+                        MT:=Ideal(WR,[ Delta_map(t)*m : t in ZBasis(T) , m in ZBasis(M) ]);
+                        assert M subset MT;
+                        sendsMtoM:=IsCoprime(Index(MT,M),p);
+                        // M = MT at p
+                    else
+                        sendsMtoM:=true;
+                    end if;
+                    if sendsItoI and sendsMtoM then
+                        Append(~end_test,T);
+                    end if;
+                end for;
+                require S eq Order(&cat[ ZBasis(T) : T in end_test ]) : "The local info provided by I and M determine and order which is different from S.";
+        end if;
     end if;
     AV:=New(AbelianVarietyFq);
     AV`IsogenyClass:=isog;
