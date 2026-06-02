43d10ab58a3a977025abb6a2720e04349323ce56
Stefano Marseglia <stefano.marseglia89@gmail.com>
2026-06-02 11:06:07 -0400

    added parallel code to recompute the examples

---
+++ tests/recompute_examples_parallel_output_01.m
@@ -0,0 +1,140 @@
+-------------------------
+2.4.a_e
+	Using slopesDieudonneModules:=(0,1) and SetAssertions(2),
+	we got 16 isomorphism classes in 1 mins 36 secs
+	GeneralizedDeligneModules computed in 0 mins 1 secs
+	<s_nu,e_nu,g_nu> = [<1/2,1,2>,<1/2,1,2>]
+	p is  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,64,0,0,4,[],false,false,[ 2 ]
+	2,32,0,0,2,[],false,false,[ 4, 3, 6, 5 ]
+	3,16,0,0,1,[],false,false,[ 7 ]
+	4,16,0,0,1,[],false,false,[ 7 ]
+	5,16,1,2,2,[ 1, 1 ],false,false,[ 9 ]
+	6,16,0,0,2,[],false,false,[ 7 ]
+	7,8,1,2,1,[ 1, 1 ],false,false,[ 8, 9, 10 ]
+	8,4,1,2,1,[ 1, 1 ],false,false,[ 13 ]
+	9,4,1,4,1,[ 2, 2, 2, 2 ],false,false,[ 13 ]
+	10,4,0,0,1,[],false,true,[ 11, 12 ]
+	11,2,0,0,1,[],false,true,[ 13 ]
+	12,2,0,0,1,[],false,true,[ 13 ]
+	13,1,1,4,1,[ 2, 2, 2, 2 ],true,true,[]
+-------------------------
+4.4.ag_s_abk_cq
+	Using slopesDieudonneModules:=(0,1) and SetAssertions(2),
+	we got 20 isomorphism classes in 7 mins 19 secs
+	GeneralizedDeligneModules computed in 0 mins 8 secs
+	<s_nu,e_nu,g_nu> = [<3/4,2,2>,<1/4,2,2>]
+	p is not  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,704,0,0,12,[],false,false,[ 2, 10 ]
+	2,352,0,0,6,[],false,false,[ 4, 3, 6, 5, 16 ]
+	3,176,0,0,3,[],false,false,[ 8, 7, 21 ]
+	4,176,0,0,6,[],false,false,[ 8, 20 ]
+	5,176,0,0,6,[],false,false,[ 9, 7, 19 ]
+	6,176,0,0,3,[],false,false,[ 8, 9, 22 ]
+	7,88,0,0,3,[],false,false,[ 13, 12, 25 ]
+	8,88,0,0,3,[],false,false,[ 14, 11, 13, 24 ]
+	9,88,0,0,3,[],false,false,[ 15, 13, 26 ]
+	10,64,0,0,1,[],false,false,[ 16 ]
+	11,44,1,2,3,[ 1, 1 ],false,true,[ 23, 31 ]
+	12,44,0,0,1,[],false,false,[ 18, 28 ]
+	13,44,0,0,3,[],false,false,[ 17, 18, 27 ]
+	14,44,1,2,1,[ 1, 1 ],false,false,[ 23, 29 ]
+	15,44,0,0,1,[],false,false,[ 17, 30 ]
+	16,32,0,0,1,[],false,false,[ 21, 20, 22, 19 ]
+	17,22,0,0,1,[],false,false,[ 23, 32 ]
+	18,22,0,0,1,[],false,false,[ 23, 33 ]
+	19,16,0,0,1,[],false,false,[ 26, 25 ]
+	20,16,0,0,1,[],false,false,[ 24 ]
+	21,16,0,0,1,[],false,false,[ 24, 25 ]
+	22,16,0,0,1,[],false,false,[ 24, 26 ]
+	23,11,1,4,1,[ 2, 2, 2, 2 ],true,true,[ 34 ]
+	24,8,0,0,1,[],false,false,[ 31, 27, 29 ]
+	25,8,0,0,1,[],false,false,[ 27, 28 ]
+	26,8,0,0,1,[],false,false,[ 30, 27 ]
+	27,4,0,0,1,[],false,false,[ 32, 33 ]
+	28,4,0,0,1,[],false,false,[ 33 ]
+	29,4,1,2,1,[ 1, 1 ],false,false,[ 34 ]
+	30,4,0,0,1,[],false,false,[ 32 ]
+	31,4,1,2,1,[ 1, 1 ],false,true,[ 34 ]
+	32,2,0,0,1,[],false,false,[ 34 ]
+	33,2,0,0,1,[],false,false,[ 34 ]
+	34,1,1,4,1,[ 2, 2, 2, 2 ],true,true,[]
+-------------------------
+3.9.ab_ad_bt
+	Using slopesDieudonneModules:=(0,1) and SetAssertions(2),
+	we got 44 isomorphism classes in 24 mins 38 secs
+	GeneralizedDeligneModules computed in 0 mins 8 secs
+	<s_nu,e_nu,g_nu> = [<0,1,1>,<1/2,1,2>,<1,1,1>]
+	p is not  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,81,0,0,60,[],false,false,[ 2 ]
+	2,27,0,0,20,[],false,false,[ 3 ]
+	3,9,1,2,20,[ 1, 1 ],false,true,[ 4 ]
+	4,1,1,2,2,[ 2, 2 ],true,true,[]
+-------------------------
+4.4.b_b_e_ae
+	Using slopesDieudonneModules:=(0,1) and SetAssertions(2),
+	we got 168 isomorphism classes in 33 mins 8 secs
+	GeneralizedDeligneModules computed in 2 mins 0 secs
+	<s_nu,e_nu,g_nu> = [<0,1,2>,<1/2,2,2>,<1,1,2>]
+	p is not  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,64,0,0,192,[],false,false,[ 2 ]
+	2,32,0,0,96,[],false,false,[ 4, 3, 6, 5 ]
+	3,16,0,0,48,[],false,false,[ 7 ]
+	4,16,0,0,48,[],false,false,[ 7 ]
+	5,16,1,2,32,[ 1, 1 ],false,false,[ 11 ]
+	6,16,0,0,48,[],false,false,[ 7 ]
+	7,8,1,2,24,[ 1, 1 ],false,false,[ 9, 10, 8, 11 ]
+	8,4,0,0,12,[],false,false,[ 12 ]
+	9,4,0,0,24,[],false,true,[ 12 ]
+	10,4,0,0,12,[],false,false,[ 12 ]
+	11,4,1,4,8,[ 2, 2, 1, 2 ],false,false,[ 13 ]
+	12,2,1,1,12,[ 1 ],false,true,[ 13 ]
+	13,1,1,3,4,[ 2, 2, 2 ],true,true,[]
---
+++ tests/recompute_examples_parallel_output_all.m
@@ -0,0 +1,175 @@
+-------------------------
+2.4.a_e
+	Using slopesDieudonneModules:=all and SetAssertions(2),
+	we got 16 isomorphism classes in 1 mins 37 secs
+	GeneralizedDeligneModules computed in 0 mins 1 secs
+	<s_nu,e_nu,g_nu> = [<1/2,1,2>,<1/2,1,2>]
+	p is  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,64,0,0,4,[],false,false,[ 2 ]
+	2,32,0,0,2,[],false,false,[ 4, 3, 6, 5 ]
+	3,16,0,0,1,[],false,false,[ 7 ]
+	4,16,0,0,1,[],false,false,[ 7 ]
+	5,16,1,2,2,[ 1, 1 ],false,false,[ 9 ]
+	6,16,0,0,2,[],false,false,[ 7 ]
+	7,8,1,2,1,[ 1, 1 ],false,false,[ 8, 9, 10 ]
+	8,4,1,2,1,[ 1, 1 ],false,false,[ 13 ]
+	9,4,1,4,1,[ 2, 2, 2, 2 ],false,false,[ 13 ]
+	10,4,0,0,1,[],false,true,[ 11, 12 ]
+	11,2,0,0,1,[],false,true,[ 13 ]
+	12,2,0,0,1,[],false,true,[ 13 ]
+	13,1,1,4,1,[ 2, 2, 2, 2 ],true,true,[]
+-------------------------
+4.4.ag_s_abk_cq
+	Using slopesDieudonneModules:=all and SetAssertions(2),
+	we got 20 isomorphism classes in 7 mins 21 secs
+	GeneralizedDeligneModules computed in 0 mins 7 secs
+	<s_nu,e_nu,g_nu> = [<3/4,2,2>,<1/4,2,2>]
+	p is not  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,704,0,0,12,[],false,false,[ 2, 10 ]
+	2,352,0,0,6,[],false,false,[ 4, 3, 6, 5, 16 ]
+	3,176,0,0,3,[],false,false,[ 8, 7, 21 ]
+	4,176,0,0,6,[],false,false,[ 8, 20 ]
+	5,176,0,0,6,[],false,false,[ 9, 7, 19 ]
+	6,176,0,0,3,[],false,false,[ 8, 9, 22 ]
+	7,88,0,0,3,[],false,false,[ 13, 12, 25 ]
+	8,88,0,0,3,[],false,false,[ 14, 11, 13, 24 ]
+	9,88,0,0,3,[],false,false,[ 15, 13, 26 ]
+	10,64,0,0,1,[],false,false,[ 16 ]
+	11,44,1,2,3,[ 1, 1 ],false,true,[ 23, 31 ]
+	12,44,0,0,1,[],false,false,[ 18, 28 ]
+	13,44,0,0,3,[],false,false,[ 17, 18, 27 ]
+	14,44,1,2,1,[ 1, 1 ],false,false,[ 23, 29 ]
+	15,44,0,0,1,[],false,false,[ 17, 30 ]
+	16,32,0,0,1,[],false,false,[ 21, 20, 22, 19 ]
+	17,22,0,0,1,[],false,false,[ 23, 32 ]
+	18,22,0,0,1,[],false,false,[ 23, 33 ]
+	19,16,0,0,1,[],false,false,[ 26, 25 ]
+	20,16,0,0,1,[],false,false,[ 24 ]
+	21,16,0,0,1,[],false,false,[ 24, 25 ]
+	22,16,0,0,1,[],false,false,[ 24, 26 ]
+	23,11,1,4,1,[ 2, 2, 2, 2 ],true,true,[ 34 ]
+	24,8,0,0,1,[],false,false,[ 31, 27, 29 ]
+	25,8,0,0,1,[],false,false,[ 27, 28 ]
+	26,8,0,0,1,[],false,false,[ 30, 27 ]
+	27,4,0,0,1,[],false,false,[ 32, 33 ]
+	28,4,0,0,1,[],false,false,[ 33 ]
+	29,4,1,2,1,[ 1, 1 ],false,false,[ 34 ]
+	30,4,0,0,1,[],false,false,[ 32 ]
+	31,4,1,2,1,[ 1, 1 ],false,true,[ 34 ]
+	32,2,0,0,1,[],false,false,[ 34 ]
+	33,2,0,0,1,[],false,false,[ 34 ]
+	34,1,1,4,1,[ 2, 2, 2, 2 ],true,true,[]
+-------------------------
+3.9.ab_ad_bt
+	Using slopesDieudonneModules:=all and SetAssertions(2),
+	we got 44 isomorphism classes in 25 mins 30 secs
+	GeneralizedDeligneModules computed in 0 mins 9 secs
+	<s_nu,e_nu,g_nu> = [<0,1,1>,<1/2,1,2>,<1,1,1>]
+	p is not  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,81,0,0,60,[],false,false,[ 2 ]
+	2,27,0,0,20,[],false,false,[ 3 ]
+	3,9,1,2,20,[ 1, 1 ],false,true,[ 4 ]
+	4,1,1,2,2,[ 2, 2 ],true,true,[]
+-------------------------
+4.4.b_b_e_ae
+	Using slopesDieudonneModules:=all and SetAssertions(2),
+	we got 168 isomorphism classes in 87 mins 55 secs
+	GeneralizedDeligneModules computed in 33 mins 30 secs
+	<s_nu,e_nu,g_nu> = [<0,1,2>,<1/2,2,2>,<1,1,2>]
+	p is not  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,64,0,0,192,[],false,false,[ 2 ]
+	2,32,0,0,96,[],false,false,[ 4, 3, 6, 5 ]
+	3,16,0,0,48,[],false,false,[ 7 ]
+	4,16,0,0,48,[],false,false,[ 7 ]
+	5,16,1,2,32,[ 1, 1 ],false,false,[ 11 ]
+	6,16,0,0,48,[],false,false,[ 7 ]
+	7,8,1,2,24,[ 1, 1 ],false,false,[ 9, 10, 8, 11 ]
+	8,4,0,0,12,[],false,false,[ 12 ]
+	9,4,0,0,24,[],false,true,[ 12 ]
+	10,4,0,0,12,[],false,false,[ 12 ]
+	11,4,1,4,8,[ 2, 2, 2, 2 ],false,false,[ 13 ]
+	12,2,1,1,12,[ 1 ],false,true,[ 13 ]
+	13,1,1,3,4,[ 2, 2, 2 ],true,true,[]
+-------------------------
+3.8.l_ci_ia
+	Using slopesDieudonneModules:=all and SetAssertions(2),
+	we got 12 isomorphism classes in 159 mins 35 secs
+	GeneralizedDeligneModules computed in 0 mins 39 secs
+	<s_nu,e_nu,g_nu> = [<0,1,1>,<1/2,2,1>,<1/2,2,1>,<1,1,1>]
+	p is  totally split in E^+
+
+For each overorder S, we print the following string of data:
+	iS = which overorder of Z[pi,q/pi]
+	[OE:S]
+	w(S) = #iso away from DM
+	d(S) = #Dieudonné modules with End S
+	h(S)=#Pic(S)
+	a numbers of the DM with End S
+	is S maximal at (0,1)?
+	does S contain O_{E^+}?
+	indices of minimal overorders
+
+	1,64,0,0,8,[],false,false,[ 2 ]
+	2,32,0,0,4,[],false,false,[ 3, 5, 4 ]
+	3,16,0,0,2,[],false,false,[ 7 ]
+	4,16,0,0,4,[],false,false,[ 7 ]
+	5,16,0,0,2,[],false,false,[ 8, 6, 7 ]
+	6,8,0,0,1,[],false,false,[ 12 ]
+	7,8,0,0,2,[],false,false,[ 12, 10, 11, 9 ]
+	8,8,0,0,1,[],false,false,[ 12 ]
+	9,4,0,0,2,[],false,true,[ 13, 15 ]
+	10,4,0,0,1,[],false,false,[ 14, 15 ]
+	11,4,0,0,2,[],false,false,[ 14, 13 ]
+	12,4,1,4,1,[ 1, 1, 1, 1 ],false,false,[ 14 ]
+	13,2,0,0,2,[],false,true,[ 16 ]
+	14,2,1,7,1,[ 2, 1, 1, 1, 1, 1, 1 ],false,false,[ 16 ]
+	15,2,0,0,1,[],false,true,[ 16 ]
+	16,1,1,1,1,[ 2 ],true,true,[]
