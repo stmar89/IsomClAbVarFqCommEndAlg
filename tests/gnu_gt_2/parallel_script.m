b9c1d73f9c31b29e98902277954070c79c8d72d1
Stefano Marseglia <stefano.marseglia89@gmail.com>
2026-06-03 06:24:23 -0400

    running test gnu > 2, found only ordinary examples

--- tests/gnu_gt_2/parallel_script.m
+++ tests/gnu_gt_2/parallel_script.m
@@ -11,31 +11,36 @@
 
     PP<x>:=PolynomialRing(Integers());
     h:=PP!(eval(cc));
-    output:=[];
-
+    times:=[];
+    
+    tt0:=Cputime();
     for method in ["(0,1)","all"] do
         if assigned isog then delete isog; end if;
         isog:=IsogenyClass(h);
-        _,a:=IsPowerOf(FiniteField(isog),CharacteristicFiniteField(isog));
-        nus0,nus01,nus1:=PlacesOfQFAbove_p(isog)
-        nus:=nus0 cat nus01 cat nus1;
-        sl:=[Slope(nu):nu in nus];
-        ParallelSort(~sl,~nus);
-        sl:=StripWhiteSpace(Sprint(sl));
-        gnus:=StripWhiteSpace(Sprint([GCD(InertiaDegree(nu),a):nu in nus]));
         t0:=Cputime();
         if assigned n_iso then n_iso_old:=n_iso; end if;
-        if IsOrdinary(isog) and method eq "(0,1)" then
-            n_iso:=#ICM(ZFVOrder(sog);
-            method_print:="ICM"
-        else
-            n_iso:=#IsomorphismClassesCommEndAlg(isog:slopesDieudonneModules:=method);
-            method_print:=method;
-        end if;
-        warning:=not assigned n_iso_old or n_iso_old eq n_iso select "" else "<------------ outputs are different!";
+        n_iso:=#IsomorphismClassesCommEndAlg(isog:slopesDieudonneModules:=method);
+        warning:=not assigned n_iso_old or n_iso_old eq n_iso select "   " else "ERR";
         t1:=Round(Cputime(t0));
-        Append(~output,Sprintf("%20o %20o %20o %5o : isom classes %3o computed in %o mins %o secs %o",label,sl,gnus,method_print,n_iso,t1 div 60,t1 mod 60,warning));
+        Append(~times,t1);
     end for;
-    printf "%o\n",Join(output,"\n");
-    fprintf file_out,"%o\n",Join(output,"\n");
+    _,a:=IsPowerOf(FiniteField(isog),CharacteristicFiniteField(isog));
+    nus0,nus01,nus1:=PlacesOfQFAbove_p(isog);
+    nus:=nus0 cat nus01 cat nus1;
+    sl:=[Slope(nu):nu in nus];
+    ParallelSort(~sl,~nus);
+    sl:=StripWhiteSpace(Sprintf("sl=%o",sl));
+    gnus:=StripWhiteSpace(Sprintf("gnus=%o",[GCD(InertiaDegree(nu),a):nu in nus]));
+    n_iso:=Sprintf("n_iso=%o",n_iso);
+    tt1:=Round(Cputime(tt0));
 
+    output:=Sprintf("tot=%2om %2os \"(0,1)\"=%2om %2os \"all\"=%2om %2os %o %9o %13o %14o %o",
+            tt1 div 60,tt1 mod 60,
+            times[1] div 60,times[1] mod 60,
+            times[2] div 60,times[2] mod 60,
+            warning,
+            n_iso,
+            sl,gnus,StripWhiteSpace(Sprint(Coefficients(h)))
+            );
+    printf "%o\n",output;
+    fprintf file_out,"%o\n",output;
