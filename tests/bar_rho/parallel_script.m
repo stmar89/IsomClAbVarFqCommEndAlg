1e0700e91a138a8148b01b0f953e821c19ba0178
Stefano Marseglia <stefano.marseglia89@gmail.com>
2026-06-12 05:03:40 -0400

    fixed parallel bar_rho

--- tests/bar_rho/parallel_script.m
+++ tests/bar_rho/parallel_script.m
@@ -28,10 +28,11 @@
     E:=DeligneAlgebra(isog);
     pi:=PrimitiveElement(E);
     pps0:=[P:P in PrimesAbove(p*R)|pi in P and not q/pi in P];
+    R_gnus_pps0:=[GCD(a,Ilog(p,Index(R,P))):P in pps0];
     PPs0,PPs01,PPs1:=PrimesOfSAbove_p(isog,WR);
+    PP0_above_pp0:=[#[PP:PP in PPs0|forall{x:x in Generators(pps0[i])|Delta_map(x) in PP}]:i in [1..#pps0]];
     conj1:=#PPs01 eq 1;
-    conj2:=forall{P:P in pps0|#[PP:PP in PPs0|forall{x:x in Generators(P)|Delta_map(x) in PP}] 
-                              eq GCD(a,InertiaDegree(P))};
+    conj2:=forall{i:i in [1..#pps0]|PP0_above_pp0[i] eq R_gnus_pps0[i]};
 
     sl01:=[Slope(nu):nu in nus01];
     ParallelSort(~sl01,~nus01);
@@ -57,7 +58,8 @@
             if is_rho_id then
                 conj3:=forall{i:i in [2..#NUs]|NUs[i] eq bar_NUs[i]};
             else
-                conj3:=IsEven(gnu) and forall{i:i in [1..#NUS]|bar_NUs[i] eq NUs[(i+(gnu div 2)) mod gnu]};
+                conj3:=IsEven(gnu) and forall{i:i in [1..gnu div 2]|bar_NUs[i] eq NUs[(i+(gnu div 2))]}
+                                   and forall{i:i in [(gnu div 2)+1..gnu]|bar_NUs[i] eq NUs[(i-(gnu div 2))]};
             end if;
             is_rho_id:=is_rho_id select "t" else "f";
             Append(~data_nus,Sprintf("[%o,%o,%o,%o]",sl,gnu,conj_st,is_rho_id));
@@ -69,9 +71,11 @@
         Append(~data_nus,Sprintf("[1,%o,f]",gnu));
     end for;
 
-    output:=Sprintf("data_nus=%o:WR_gnus_pps0=%o:conj1=%o:conj2=%o:conj3=%o:s=%o",
+    output:=Sprintf("a=%o:data_nus=%o:PP0_above_pp0=%o:R_gnus_pps0=%o:conj1=%o:conj2=%o:conj3=%o:s=%o",
+            a,
             sprint(data_nus),
-            sprint([GCD(a,InertiaDegree(P)):P in pps0]),
+            sprint(PP0_above_pp0),
+            sprint(R_gnus_pps0),
             conj1 select "t" else "f",
             conj2 select "t" else "f",
             conj3 select "t" else "f",
