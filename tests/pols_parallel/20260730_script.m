bf607d7e7704460b3e98af480258aae2a79a4978
Stefano Marseglia <stefano.marseglia89@gmail.com>
2026-07-30 11:14:18 -0400

    output from parallel removed from gitignore

--- tests/pols_parallel/20260730_script.m
+++ tests/pols_parallel/20260730_script.m
@@ -3,7 +3,7 @@
 
     Use branch feature-bar_dual_pols on IsomClAbVarFqCommEndAlg
 
-    parallel -j 60 --timeout 3600 --resume-failed \
+    parallel -j 20 --timeout 7200 --resume-failed \
         --joblog ~/IsomClAbVarFqCommEndAlg/tests/pols_parallel/joblog \
         -a ~/IsomClAbVarFqCommEndAlg/tests/pols_parallel/weil_poly_sqfree_notFp_notord_notalmord.txt \
         magma -b s:={} ~/IsomClAbVarFqCommEndAlg/tests/pols_parallel/20260730_script.m 
