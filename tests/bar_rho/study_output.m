de5a277cdf7bec517cc3e6d531878429289df00f
stmar89 <stefano.marseglia89@gmail.com>
2026-08-06 12:12:14 +0200

    added tests with higher g_nu

--- tests/bar_rho/study_output.m
+++ tests/bar_rho/study_output.m
@@ -20,50 +20,83 @@
         end if;
      end for;
      */
-     function ReplaceAll(s, pattern, replacement)
-        res := "";
-        p := Position(s, pattern);
-
-        while p gt 0 do
-            // Append the text up to the pattern + the replacement
-            res := res cat s[1 .. p-1] cat replacement;
-            // Advance past the matched pattern
-            s := s[p + #pattern .. #s];
+     function ReplaceAll(s0, patterns, replacements)
+        s:=s0;
+        for i in [1..#patterns] do
+            pattern:=patterns[i];
+            replacement:=replacements[i];
+            res := "";
             p := Position(s, pattern);
-        end while;
-
-        // Append any remaining trailing characters
-    return res cat s;
+            while p gt 0 do
+                // Append the text up to the pattern + the replacement
+                res := res cat s[1 .. p-1] cat replacement;
+                // Advance past the matched pattern
+                s := s[p + #pattern .. #s];
+                p := Position(s, pattern);
+            end while;
+            // Append any remaining trailing characters
+            s:=res cat s;
+        end for;
+        return s;
     end function;
+
     conj_pair:=AssociativeArray(:Default:=0);
     rho_id:=AssociativeArray(:Default:=0);
     rho_notid:=AssociativeArray(:Default:=0);
-     for line in lines do
+    max_conj_pair:=AssociativeArray(:Default:=[]);
+    max_rho_id:=AssociativeArray(:Default:=[]);
+    max_rho_notid:=AssociativeArray(:Default:=[]);
+    for line in lines do
          all:=Split(line,":");
          a:=StringToInteger(Split(all[1],"=")[2]);
+         cc:=Split(all[#all],"=")[2];
          data_nus:=Split(all[2],"=")[2];
-         data_nus:=ReplaceAll(data_nus,"[","<");
-         data_nus:=ReplaceAll(data_nus,"]",">");
-         data_nus:=ReplaceAll(data_nus,"t","true");
-         data_nus:=ReplaceAll(data_nus,"f","false");
+         data_nus:=ReplaceAll(data_nus,["[","]","t","f"],["<",">","true","false"]);
          data_nus:=eval(data_nus);
          for nu in data_nus do
              if not nu[3] then
-                 conj_pair[a]:=Max(conj_pair[a],nu[2]);
+                 if nu[2] gt conj_pair[a] then
+                     conj_pair[a]:=nu[2];
+                     max_conj_pair[a]:={@ cc @}; // we reset it
+                 elif nu[2] eq conj_pair[a] then
+                     Include(~max_conj_pair[a],cc);
+                 end if;
              elif nu[4] then
-                rho_id[a]:=Max(rho_id[a],nu[2]);
+                 if nu[2] gt rho_id[a] then
+                     rho_id[a]:=nu[2];
+                     max_rho_id[a]:={@ cc @}; // we reset it
+                 elif nu[2] eq rho_id[a] then
+                     Include(~max_rho_id[a],cc);
+                 end if;
              else
-                rho_notid[a]:=Max(rho_notid[a],nu[2]);
+                 if nu[2] gt rho_notid[a] then
+                     rho_notid[a]:=nu[2];
+                     max_rho_notid[a]:={@ cc @}; // we reset it
+                 elif nu[2] eq rho_notid[a] then
+                     Include(~max_rho_notid[a],cc);
+                 end if;
              end if;
           end for;
      end for;
+     printf "conj_pair\n";
      for a->M in conj_pair do
-         a,M;
+         a,M,#max_conj_pair[a];
      end for;
+     printf "rho_id\n";
      for a->M in rho_id do
-         a,M;
+         a,M,#max_rho_id[a];
      end for;
+     printf "rho_notid\n";
      for a->M in rho_notid do
-         a,M;
+         a,M,#max_rho_notid[a];
      end for;
 
+     // We take the maximal g_nus and break even by taking th minimal a
+     // For rho_id we get at most only g_nu=2 :-( not very interesting.
+     for cc in max_conj_pair[3] do
+         fprintf "~/IsomClAbVarFqCommEndAlg/tests/bar_rho/conj_pair_a_3_gnu_3.txt","%o\n",cc;
+     end for;
+     for cc in max_rho_notid[4] do
+         fprintf "~/IsomClAbVarFqCommEndAlg/tests/bar_rho/rho_notid_a_4_gnu_4.txt","%o\n",cc;
+     end for;
+     
---
+++ tests/semilin_ops_parallel/20260806_script.m
@@ -0,0 +1,76 @@
+/* 
+
+    Use branch feature-bar_dual_pols on IsomClAbVarFqCommEndAlg
+
+    parallel --timeout --resume-failed \
+        --joblog ~/IsomClAbVarFqCommEndAlg/tests/semilin_ops_parallel/joblog \
+        -a ~/IsomClAbVarFqCommEndAlg/tests/semilin_ops_parallel/input.m \
+        magma -b s:={} ~/IsomClAbVarFqCommEndAlg/tests/semilin_ops_parallel/20260806_script.m 
+
+*/
+
+    SetAssertions(2);
+    SetColumns(0);
+    AttachSpec("~/AbVarFq/spec");
+    //AttachSpec("~/AlgEt/spec");
+    AttachSpec("~/AlgEt/specMod");
+    AttachSpec("~/AlgEt/specMtrx");
+    AttachSpec("~/IsomClAbVarFqCommEndAlg/spec");
+    Attach("~/PolarizationsFq/magma/misc.m");
+
+    fld:="~/IsomClAbVarFqCommEndAlg/tests/rho_notid_a_4_gnu_4.txt/";
+    run:="20260806_";
+    
+    ok_file:=fld*run*"ok.txt";
+    issue_file:=fld*run*"issues.txt";
+
+    //SetDebugOnError(true);
+    //SetVerbose("alpha_at_precision",2);
+
+    PP<x>:=PolynomialRing(Integers());
+    SetAssertions(2);
+    AttachSpec("~/AbVarFq/spec");
+    AttachSpec("~/AlgEt/specMod");
+    AttachSpec("~/AlgEt/specMtrx");
+    AttachSpec("~/IsomClAbVarFqCommEndAlg/spec");
+    all:=Split(Read("~/IsomClAbVarFqCommEndAlg/tests/weil_poly_sq_not_prime-ord-almord.txt"));
+    m0:=10;
+    for s in all do
+        try
+            cc:=[StringToInteger(c):c in Split(s,"[,]")];
+            g:=(#cc-1) div 2;
+            q:=Round(cc[1]^(1/g));
+            _,p,a:=IsPrimePower(q);
+            h:=PP!cc;
+            ccs:=StripWhiteSpace(Sprint(cc));
+            isog:=IsogenyClass(h);
+            conj_pairs,rho_id,rho_notid:=SortPlacesOfQFAbove_p(isog);
+            _,_,_,_,_,_,OA,_,WR:=DieudonneAlgebraCommEndAlg(isog);
+            out_str:=Sprintf("%o,%o,%o\ta=%o\t[OA:JOA]=",#conj_pairs,#rho_id,#rho_notid,a)
+            plE0,plE01,plE1:=PlacesOfQFAbove_p(isog);
+            plE:=plE0 cat plE01 cat plE1;
+            plA:=&cat[PlacesOfDieudonneAlgebraSortedBySigmaAbovePlaceOfQF(isog,nu):nu in plE];
+            inds:=[];
+            Js:=[];
+            for exps in ExponentsDual(isog) do
+                assert #plA eq #exps;
+                JOA:=&*[ plA[i]^exps[i] : i in [1..#exps] ]; 
+                ind:=Index(OA,JOA);
+                test,n:=IsPowerOf(ind,p);
+                assert test;
+                J:=WR!!JOA;
+                ZBasisLLL(J);
+                out_str cat:=Sprintf("%o^%o ",p,n);
+                Append(~Js,J);
+            end for;
+            out_str cat:=Sprintf "%o",ccs;
+            for J in Js do
+                _:=SemilinearOperatorsDualComp(isog,J,m0);
+            end for;
+            fprintf ok_file,"%o\n",out_str;
+        catch e
+            fprintf issue_file,"%o %o\n",out_str,e`Position;
+        end try;
+    end for;
+
+    quit;
